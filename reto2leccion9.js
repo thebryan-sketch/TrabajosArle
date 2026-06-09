9c63687d1a7485b7f6ae87d9a90cd5ead8ed22fa
Brayan Andrés Castañeda Rendón <brayitan1091@gmail.com>
2026-06-09 16:47:12 -0500

    Create reto2leccion9.js

---
+++ reto2leccion9.js
@@ -0,0 +1,19 @@
+function reiniciarContador() {
+    console.log("Contador reiniciado a 0");
+}
+
+function notificarUsuario() {
+    console.log("🔔 Notificación enviada");
+}
+
+function cerrarSesion() {
+    console.log("👋 Sesión cerrada");
+}
+
+function ejecutarLimpieza() {
+    reiniciarContador();
+    notificarUsuario();
+    cerrarSesion();
+}
+
+ejecutarLimpieza();
