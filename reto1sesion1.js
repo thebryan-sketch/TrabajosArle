2f3dd2a2e01279245ab4697ec1bdcf612c38f402
Brayan Andrés Castañeda Rendón <brayitan1091@gmail.com>
2026-06-09 16:02:30 -0500

    primer reto 1

---
+++ reto1sesion1.js
@@ -0,0 +1,13 @@
+const titulo = "Cien años de soledad";
+const autor = "Gabriel García Márquez";
+const añoPublicacion = 1967;
+const paginas = 496;
+const disponible = true;
+
+console.log(titulo, typeof titulo);
+console.log(autor, typeof autor);
+console.log(añoPublicacion, typeof añoPublicacion);
+console.log(paginas, typeof paginas);
+console.log(disponible, typeof disponible);
+
+console.log(`Libro: ${titulo}, Autor: ${autor}, Año: ${añoPublicacion}, Páginas: ${paginas}, Disponible: ${disponible}`);
